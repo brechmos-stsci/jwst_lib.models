d04e1e91229a5089588e3511d29e6895c84679a7
rij <rij@560b4ebf-6bc0-4cc5-b8e0-b136f69d22d4>
2014-09-03 18:40:55 +0000

    Added dq and dq_def attributes to the saturation model, with dynamic dq masking.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/jwst/trunk/jwst_lib/models@2972 560b4ebf-6bc0-4cc5-b8e0-b136f69d22d4

--- jwst_lib/models/saturation.py
+++ jwst_lib/models/saturation.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals, division, print_function
 
 from . import model_base
+from .dynamicdq import dynamic_mask
 
 __all__ = ['SaturationModel']
 
@@ -10,8 +11,16 @@ class SaturationModel(model_base.DataModel):
     """
     schema_url = "saturation.schema.json"
 
-    def __init__(self, init=None, sat=None, **kwargs):
+    def __init__(self, init=None, sat=None, dq=None, dq_def=None, **kwargs):
         super(SaturationModel, self).__init__(init=init, **kwargs)
 
         if sat is not None:
             self.sat = sat
+
+        if dq is not None:
+            self.dq = dq
+
+        if dq_def is not None:
+            self.dq_def = dq_def
+
+        self.dq = dynamic_mask(self)
